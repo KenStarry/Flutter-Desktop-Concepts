a5c1219826a28b465c4bb5a0d2fee806d5b631ee
KenStarry <starrycodes@gmail.com>
2024-05-05 20:33:03 +0300

    Added Window options for more fine-tuned window control

--- truck_bidding_concept/lib/main.dart
+++ truck_bidding_concept/lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:truck_bidding_concept/core/presentation/widgets/responsive_layout.dart';
 import 'package:truck_bidding_concept/feature_dashboard/presentation/pages/dashboard_page_desktop.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions options = const WindowOptions(
+      size: Size(1300, 900),
+      center: true,
+      backgroundColor: Colors.red,
+      skipTaskbar: false,
+      windowButtonVisibility: false,
+      titleBarStyle: TitleBarStyle.normal,
+  );
+
+  windowManager.waitUntilReadyToShow(options, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
