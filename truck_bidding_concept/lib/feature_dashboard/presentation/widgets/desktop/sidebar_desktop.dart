809bf6345b117faafdd11a8c78e9f157b40d0cf3
KenStarry <starrycodes@gmail.com>
2024-05-05 19:39:07 +0300

    Sidebar desktop implementation

--- truck_bidding_concept/lib/feature_dashboard/presentation/widgets/desktop/sidebar_desktop.dart
+++ truck_bidding_concept/lib/feature_dashboard/presentation/widgets/desktop/sidebar_desktop.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:truck_bidding_concept/theme/colors.dart';
 
 class SideBarDesktop extends StatefulWidget {
   const SideBarDesktop({super.key});
@@ -13,7 +15,36 @@ class _SideBarDesktopState extends State<SideBarDesktop> {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      color: Colors.red,
+      padding: const EdgeInsets.symmetric(vertical: 32),
+      color: greenBackground,
+      child: CustomScrollView(
+        slivers: [
+          //  title
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 32),
+            sliver: SliverToBoxAdapter(
+              child: Text.rich(TextSpan(children: [
+                TextSpan(
+                    text: 'Truck',
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.titleLarge!.fontSize,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white)),
+                TextSpan(
+                    text: ' Bidding',
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.titleLarge!.fontSize,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white)),
+              ])),
+            ),
+          )
+          //  navigation
+          //  create bid button
+        ],
+      ),
     );
   }
 }
